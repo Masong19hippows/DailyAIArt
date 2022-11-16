9cf054fe605de0a55a5a078a37d37bd0c09b0891
Mason Garten <garten323@gmail.com>
2022-11-16 09:56:14 -0600

    Initial commit

---
+++ background.js
@@ -0,0 +1,11 @@
+
+
+
+chrome.wallpaper.setWallpaper(
+  {
+    'url': 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fapod.nasa.gov%2Fapod%2Fimage%2F2202%2FAuroraPillars_Correia_4898.jpg&imgrefurl=https%3A%2F%2Fapod.nasa.gov%2Fapod%2Fap220208.html&tbnid=I47E5zEVdN-joM&vet=12ahUKEwi4lqfGh7P7AhUGD98KHQpAAu8QMygHegUIARDGAQ..i&docid=ZUlRqjbz2TDaIM&w=4898&h=3265&q=image&ved=2ahUKEwi4lqfGh7P7AhUGD98KHQpAAu8QMygHegUIARDGAQ',
+    'layout': 'CENTER_CROPPED',
+    'filename': 'test_wallpaper'
+  },
+  function() {}
+);
